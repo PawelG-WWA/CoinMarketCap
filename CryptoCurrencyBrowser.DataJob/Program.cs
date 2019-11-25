940a0463528b0e52be9a6741f397745f4017c242
Pawel <pawel.a.galas@gmail.com>
2019-11-25 20:05:27 +0100

    Add project files.

---
+++ CryptoCurrencyBrowser.DataJob/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CryptoCurrencyBrowser.DataJob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
